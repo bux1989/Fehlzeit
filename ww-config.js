2b0acc545b080131e85004452226dd30130ee022
bux1989 <alexfbuckle@gmail.com>
2025-08-24 12:08:14 +0200

    Update ww-config.js

--- ww-config.js
+++ ww-config.js
@@ -16,7 +16,92 @@ export default {
       type: 'Array',
       defaultValue: [],
       bindable: true,
-      section: 'data'
+      section: 'data',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'array',
+        tooltip: 'An array of absence data objects'
+      },
+      propertyHelp: {
+        tooltip: 'The absence data to display in the dashboard'
+      }
+      /* wwEditor:end */
+    },
+    availableClasses: {
+      label: {
+        en: 'Available Classes',
+        de: 'Verfügbare Klassen'
+      },
+      type: 'Array',
+      defaultValue: [],
+      bindable: true,
+      section: 'data',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'array',
+        tooltip: 'An array of available class objects'
+      },
+      propertyHelp: {
+        tooltip: 'The list of available classes to filter by'
+      }
+      /* wwEditor:end */
+    },
+    currentUser: {
+      label: {
+        en: 'Current User',
+        de: 'Aktueller Benutzer'
+      },
+      type: 'Object',
+      defaultValue: null,
+      bindable: true,
+      section: 'data',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'object',
+        tooltip: 'The current user object'
+      },
+      propertyHelp: {
+        tooltip: 'The current user information for permission checks'
+      }
+      /* wwEditor:end */
+    },
+    isLoading: {
+      label: {
+        en: 'Loading State',
+        de: 'Ladezustand'
+      },
+      type: 'OnOff',
+      defaultValue: false,
+      bindable: true,
+      section: 'states',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'boolean',
+        tooltip: 'Whether the dashboard is in loading state'
+      },
+      propertyHelp: {
+        tooltip: 'Controls the loading state of the dashboard'
+      }
+      /* wwEditor:end */
+    },
+    error: {
+      label: {
+        en: 'Error Message',
+        de: 'Fehlermeldung'
+      },
+      type: 'Text',
+      defaultValue: '',
+      bindable: true,
+      section: 'states',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'string',
+        tooltip: 'Error message to display'
+      },
+      propertyHelp: {
+        tooltip: 'Error message to display when an error occurs'
+      }
+      /* wwEditor:end */
     },
     readonly: {
       label: {
@@ -26,7 +111,54 @@ export default {
       type: 'OnOff',
       defaultValue: false,
       bindable: true,
-      section: 'settings'
+      section: 'settings',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'boolean',
+        tooltip: 'Whether the dashboard is in readonly mode'
+      },
+      propertyHelp: {
+        tooltip: 'Controls whether users can edit data in the dashboard'
+      }
+      /* wwEditor:end */
+    },
+    loadingText: {
+      label: {
+        en: 'Loading Text',
+        de: 'Ladetext'
+      },
+      type: 'Text',
+      defaultValue: 'Lädt Daten...',
+      bindable: true,
+      section: 'texts',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'string',
+        tooltip: 'Text to display during loading'
+      },
+      propertyHelp: {
+        tooltip: 'The text shown while data is loading'
+      }
+      /* wwEditor:end */
+    },
+    emptyText: {
+      label: {
+        en: 'Empty State Text',
+        de: 'Text für leeren Zustand'
+      },
+      type: 'Text',
+      defaultValue: 'Keine Fehlzeiten vorhanden',
+      bindable: true,
+      section: 'texts',
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'string',
+        tooltip: 'Text to display when no data is available'
+      },
+      propertyHelp: {
+        tooltip: 'The text shown when there is no absence data to display'
+      }
+      /* wwEditor:end */
     }
   },
   triggerEvents: [
@@ -35,7 +167,48 @@ export default {
       label: {
         en: 'Add Entry',
         de: 'Eintrag hinzufügen'
-      }
+      },
+      event: {}
+    },
+    {
+      name: 'editEntry',
+      label: {
+        en: 'Edit Entry',
+        de: 'Eintrag bearbeiten'
+      },
+      event: { entry: {} }
+    },
+    {
+      name: 'deleteEntry',
+      label: {
+        en: 'Delete Entry',
+        de: 'Eintrag löschen'
+      },
+      event: { entryId: '' }
+    },
+    {
+      name: 'filterChange',
+      label: {
+        en: 'Filter Changed',
+        de: 'Filter geändert'
+      },
+      event: { filters: {} }
+    },
+    {
+      name: 'sortChange',
+      label: {
+        en: 'Sort Changed',
+        de: 'Sortierung geändert'
+      },
+      event: { sortConfig: {} }
+    },
+    {
+      name: 'error',
+      label: {
+        en: 'Error Occurred',
+        de: 'Fehler aufgetreten'
+      },
+      event: { error: '' }
     }
   ]
 };
